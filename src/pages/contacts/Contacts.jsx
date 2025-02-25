3496751f881f6882e7298d7c2d8bc412196500aa
Marta Markiv <mpat@keplercode.com>
2025-02-25 14:16:13 +0200

    Updated Contacts page

--- src/pages/contacts/Contacts.jsx
+++ src/pages/contacts/Contacts.jsx
@@ -13,28 +13,39 @@ export default function Contacts() {
           <div>
             <div className="extra-big">Зв'язатися з</div>
             <div className="contacts-description">
-              Заплануйте консультацію або отримайте додаткову інформацію від наших
-              експертів щодо корпоративних послуг і офшорних рішень.
+              Заплануйте консультацію або отримайте додаткову інформацію від
+              наших експертів щодо корпоративних послуг і офшорних рішень.
             </div>
           </div>
           <div className="company-name">LionsInLaw</div>
         </Flex>
       </Flex>
-      <Flex align="start" justify="space-between" className="law-contacts-wrapper">
+      <Flex
+        align="start"
+        justify="space-between"
+        className="law-contacts-wrapper"
+      >
         <div>
           <p className="title">
             Контакти <span className="selected">LionsInLaw</span>
           </p>
-          <p className="description">Швидкий і зручний зв'язок для вирішення ваших питань</p>
-          {
-            config.contactPhones.map((item, index) => <Flex key={`${item.flag}:${index}`} align="center" justify="space-between" className="phone-wrapper">
+          <p className="description">
+            Швидкий і зручний зв'язок для вирішення ваших питань
+          </p>
+          {config.contactPhones.map((item, index) => (
+            <Flex
+              key={`${item.flag}:${index}`}
+              align="center"
+              justify="space-between"
+              className="phone-wrapper"
+            >
               <Flex align="center">
                 <Icon name={`${item.flag}-icon`} />
                 <span>{item.name}</span>
               </Flex>
               <span>{item.phone}</span>
-            </Flex>)
-          }
+            </Flex>
+          ))}
         </div>
         <div>
           <Form
@@ -44,8 +55,16 @@ export default function Contacts() {
             className="contact-us-form"
           >
             <p className="form-title">Будемо на зв'язку</p>
-            <Flex align="center" justify="space-between" className="form-item-wrapper">
-              <Form.Item label="Що ми можемо вам допомогти" name="theme" required={true}>
+            <Flex
+              align="center"
+              justify="space-between"
+              className="form-item-wrapper"
+            >
+              <Form.Item
+                label="Що ми можемо вам допомогти"
+                name="theme"
+                required={true}
+              >
                 <Select placeholder="Виберіть Теми">
                   <Select.Option value="theme1">тема 1</Select.Option>
                   <Select.Option value="theme2">тема 2</Select.Option>
@@ -57,17 +76,16 @@ export default function Contacts() {
               <Form.Item
                 name="username"
                 label="Повне ім'я"
-                rules={[
-                  { required: true, message: "Please input your name!" },
-                ]}
+                rules={[{ required: true, message: "Please input your name!" }]}
               >
-                <Input
-                placeholder="Повне ім'я"
-                />
+                <Input placeholder="Повне ім'я" />
               </Form.Item>
-
             </Flex>
-            <Flex align="center" justify="space-between" className="form-item-wrapper">
+            <Flex
+              align="center"
+              justify="space-between"
+              className="form-item-wrapper"
+            >
               <Form.Item
                 name="email"
                 label="Адреса електронної пошти"
@@ -79,44 +97,50 @@ export default function Contacts() {
                   },
                 ]}
               >
-                <Input
-                  type="email"
-                  placeholder="Адреса електронної пошти"
-                />
+                <Input type="email" placeholder="Адреса електронної пошти" />
               </Form.Item>
 
-              <Form.Item
-                name="skypeId"
-                label="Skype ID"
-              >
-                <Input placeholder="Skype ID"
-                />
+              <Form.Item name="skypeId" label="Skype ID">
+                <Input placeholder="Skype ID" />
               </Form.Item>
             </Flex>
-            <Flex align="center" justify="space-between" className="form-item-wrapper">
-              <Form.Item
-                label="Тема"
-                name="country"
-              >
-                <Input
-                />
+            <Flex
+              align="center"
+              justify="space-between"
+              className="form-item-wrapper"
+            >
+              <Form.Item label="Тема" name="country">
+                <Input />
               </Form.Item>
             </Flex>
             <Form.Item label="Повідомлення" required={true}>
               <TextArea rows={4} />
             </Form.Item>
-            <Form.Item label="File">
-              <Input type="file" />
+            <Form.Item
+              label="Прикріпити файли"
+              layout="horizontal"
+              className="file-form-item"
+            >
+              <Input type="file" placeholder="Виберіть Файли" />
             </Form.Item>
+            <p className="file-remark">
+              (*.jpg, *.jpeg, *.png, *.gif, *.bmp, *.pdf, *.txt, *.doc, *.docx,
+              *.xls, *.xlsx, {"<"}
+              {"="} 20 МБ/файл , обмежити 3 файли)
+            </p>
             <Form.Item>
-              <Checkbox onChange={()=>{}}>Електронна пошта та адреса для виставлення рахунків залишаються незмінними</Checkbox>;
+              <Checkbox onChange={() => {}} className="checkbox-form-item">
+                Електронна пошта та адреса для виставлення рахунків залишаються
+                незмінними;
+              </Checkbox>
             </Form.Item>
             <Form.Item>
               <Button
                 type="primary"
                 htmlType="submit"
                 name="submitButton"
-                onClick={()=>{}}
+                className="submit-button"
+                onClick={() => {}}
               >
                 Зв'язатися з нами
               </Button>
