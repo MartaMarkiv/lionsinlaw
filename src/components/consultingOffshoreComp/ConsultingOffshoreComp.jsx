aaf199286324c04ef920765f1421b32c6bf1b471
Marta Markiv <mpat@keplercode.com>
2025-02-27 23:50:14 +0200

    Added create company on Isla man

---
+++ src/components/consultingOffshoreComp/ConsultingOffshoreComp.jsx
@@ -0,0 +1,39 @@
+import { Flex } from "antd";
+import "./style.scss";
+import Icon from "../iconComponent/Icon";
+import LinkComponent from "../linkComponent/LinkComponent";
+
+export default function ConsultingOffshoreComp() {
+  return <div className="consulting-offshore-comp-wrapper">
+    <p className="header">Отримайте експертну консультацію щодо створення офшорної компанії</p>
+    <Flex justify="space-between">
+      <Flex vertical={true}>
+        <p>Підтримка</p>
+        <div><span>24/</span><span>7</span></div>
+      </Flex>
+      <Flex vertical={true}>
+        <div><span>$0</span><span>/ Місяць</span></div>
+        <div>
+          <Flex align="center">
+            <Icon name="ok-rounded-white"/>
+            <p>Виконаня протягом  2 дня</p>
+          </Flex>
+          <Flex align="center">
+            <Icon name="ok-rounded-white"/>
+            <p>100 % успішність</p>
+          </Flex>
+          <Flex align="center">
+            <Icon name="ok-rounded-white"/>
+            <p>Швидко, легко та з max конфіденційністю через
+            захищені системи</p>
+          </Flex>
+        </div>
+      </Flex>
+      <Flex vertical={true} justify="space-between">
+        <p className="title">LionsInLaw</p>
+        <p className="description">Дізнайтеся про вартість і умови реєстрації вашої майбутньої офшорної компанії від наших експертів</p>
+        <LinkComponent linkLabel="Зареєструвати компанію" linkValue={"/"}/>
+      </Flex>
+    </Flex>
+  </div>
+}
---
+++ src/pages/manIslandCompany/ManIslandCompany.jsx
@@ -0,0 +1,25 @@
+import { Flex } from "antd";
+import "./style.scss";
+
+export default function ManIslandCompany() {
+  return <div className="isle-man-company">
+    <Flex justify="space-between" className="banner-wrapper">
+      <div>
+        <Flex vertical={true}>
+          <p>Створіть вашу офшорну компанію на  </p>
+          <p>острові Мен</p>
+        </Flex>
+        <p>
+          Оптимізуйте свій бізнес з нашими експертними послугами реєстрації компаній на Острові Мен. Глобальні можливості, податкові переваги та надійний партнер 
+        </p>
+      </div>
+      <div>
+        <p>LionsInLaw</p>
+        <div>
+          <p>1000+</p>
+          <p>задоволених клієнтів</p>
+        </div>
+      </div>
+    </Flex>
+  </div>
+}
