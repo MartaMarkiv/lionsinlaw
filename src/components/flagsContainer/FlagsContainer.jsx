5003b83d46923adc45d09693cdcf6741e9766e87
Marta Markiv <mpat@keplercode.com>
2025-03-06 10:17:09 +0200

    Added running line for flags

--- src/components/flagsContainer/FlagsContainer.jsx
+++ src/components/flagsContainer/FlagsContainer.jsx
@@ -1,13 +1,16 @@
 import { Flex } from "antd";
 import "./style.scss";
 import config from "../../config";
+import Marquee from "react-fast-marquee";
 
 export default function FlagsContainer() {
   return (
-    <Flex className="flags-container" justify="space-between">
+    // <Flex className="flags-container" justify="space-between">
+    <Marquee className="flags-container">
       {config.nameFlags.map((itemFlag, index) => (
         <div key={`${index}:${itemFlag}`} className={`flag-item ${itemFlag}`} />
       ))}
-    </Flex>
+      </Marquee>
+    // </Flex>
   );
 }
